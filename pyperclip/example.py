8182ab49fe84e87426759e283e7a9327ac59a237
Mastermind-sap <66560136+Mastermind-sap@users.noreply.github.com>
2020-11-14 04:31:42 +0530

    pyperclip

---
+++ pyperclip/example.py
@@ -0,0 +1,6 @@
+import pyperclip 
+pyperclip.copy("Hello world !") 
+pyperclip.paste() 
+  
+pyperclip.copy("Anything you copy to clipboard with ctrl+c can be pasted using this module") 
+pyperclip.paste() 
